2180699665b137ae8212b7d6fee26f8dd3547d2e
dhikrama creator <cs@whanadiva.com>
2020-04-19 18:46:33 +0700

    perubahan route

---
+++ resources/views/pages/layout/header.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="logos">
+            <h1>Dhikrama Web</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="/"></a>Home</li>
+                <li><a href="/"></a>About Us</li>
+                <li><a href="/"></a>Contact Us</li>
+                <li><a href="/"></a>Services</li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
