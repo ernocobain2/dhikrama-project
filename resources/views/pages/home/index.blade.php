73d55cab3d326a50677b724dfbe08c32972ef89a
dhikrama creator <cs@whanadiva.com>
2020-04-20 22:45:10 +0700

    Penambahan link css

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,6 +1,7 @@
 @extends('pages.layout.header')
 @section('css')
 <link rel="stylesheet" href="css/slider.css" async="true">
+@endsection
 @section('main-content')
 @include('pages.home.slider')
 @endsection
